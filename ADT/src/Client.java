1b688d09051cffd5bf29fbf783a532f57b4c029e
stivvo <stefano.entatti@gmail.com>
2019-05-10 16:38:15 +0000

    client added

--- ADT/src/Client.java
+++ ADT/src/Client.java
@@ -1,20 +1,36 @@
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import java.net.Socket;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Client {
 
 	private JFrame frame;
+	
+	int[] cont = {0, 0, 0};
+	Socket s;
+	public PrintWriter pr;
 
 	/**
 	 * Launch the application.
 	 */
+	public void stampa(int i)
+	{
+		pr.println(cont[0]);
+		cont[0]++;
+		pr.flush();
+	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					Client window = new Client();
 					window.frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -27,6 +43,25 @@ public class Client {
 	 */
 	public Client() {
 		initialize();
+		
+		try
+		{
+			s = new Socket("localhost", 4999);
+		} 
+		catch (IOException e)
+		{
+			System.out.println("ERROR IN SOCKET");
+		}
+		
+		try
+		{
+			pr = new PrintWriter(s.getOutputStream());
+		} 
+		catch (IOException e)
+		{
+			System.out.println("ERROR IN PRINTWRITER");
+		}
+		
 	}
 
 	/**
@@ -34,8 +69,39 @@ public class Client {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 360, 236);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnA = new JButton("A");
+		btnA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stampa(0);
+			}
+		});
+		btnA.setBounds(12, 97, 98, 25);
+		frame.getContentPane().add(btnA);
+		
+		JButton btnB = new JButton("B");
+		btnB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stampa(1);
+			}
+		});
+		btnB.setBounds(122, 97, 98, 25);
+		frame.getContentPane().add(btnB);
+		
+		JButton btnC = new JButton("C");
+		btnC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stampa(2);
+			}
+		});
+		btnC.setBounds(232, 97, 98, 25);
+		frame.getContentPane().add(btnC);
+		
+		JLabel lblScegliereSportelloDa = new JLabel("Scegliere sportello da prenotare: ");
+		lblScegliereSportelloDa.setBounds(12, 12, 326, 37);
+		frame.getContentPane().add(lblScegliereSportelloDa);
 	}
-
 }
