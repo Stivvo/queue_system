ccd8f7236828203521644001a83a0c6aba357b3c
stivvo <stefano.entatti@gmail.com>
2019-05-17 18:04:32 +0000

    general Counter

--- QueueSystem/src/Counter.java
+++ QueueSystem/src/Counter.java
@@ -16,14 +16,42 @@ public class Counter
 	private JFrame frame;
 	private Socket s;
 	private PrintWriter p;
+	
+	private char type;
+	private int num;
+	private String name;
+	
+	public int getNum() {return num;}
+	public char getType() {return type;}
+	public String getName() {return name;}
 
 	public static void main(String[] args) 
 	{
+		char Type = args[0].charAt(0);
+		int Num = args[1].charAt(0);
+		String[] Name = new String[1];
+		
+		switch(Type)
+		{
+		case 'A':
+			Name[0] = "1. Finance";
+			break;
+		case 'B':
+			Name[0] = "2. Comunication";
+			break;
+		case 'C':
+			Name[0] = "3. Package";
+			break;
+		case 'D':
+			Name[0] = "4. Multipurpose";
+			break;
+		}//end switch Type
+		
 		System.out.println("MAIN sportelloAvanzato");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Counter window = new Counter();
+					Counter window = new Counter(Type, Num, Name);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,26 +59,22 @@ public class Counter
 			}
 		});
 	}
-
-	public Counter() throws UnknownHostException, IOException {
+	
+	public Counter(char type, int num, String[] name) throws UnknownHostException, IOException {
 		initialize();
+		
+		this.num = num;
+		this.type = type;
+		this.name = name[0];
 		
 		s = new Socket("localhost", 8045);
 		p = new PrintWriter(s.getOutputStream());
 	}
-
-	void fKey(KeyEvent e)
+	
+	private void stampa()
 	{
-		char c = e.getKeyChar();
-		
-		if (c < '1' || c > '5')
-			e.consume();
-		else
-		{
-			c = (char) ((int)c - 17); //stampa una lettera
-			p.print(c);
-			p.flush();
-		}
+		p.print(this.getType());
+		p.flush();
 	}
 	
 	private void initialize() throws UnknownHostException, IOException {
@@ -62,8 +86,7 @@ public class Counter
 			public void windowClosing(WindowEvent e) {
 				try {
 					s.close();
-				} catch (IOException e1) {
-					
+				} catch (IOException e1) {			
 					e1.printStackTrace();
 				}
 			}
@@ -72,93 +95,26 @@ public class Counter
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnA = new JButton("1. Finance");
-		btnA.addActionListener(new ActionListener() {
+		JButton btn = new JButton(this.getName());
+		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				p.print('A');
-				p.flush();
+				stampa();
 			}
 		});
-		btnA.setBounds(159, 38, 105, 27);
-		frame.getContentPane().add(btnA);
+		btn.setBounds(159, 38, 105, 27);
+		frame.getContentPane().add(btn);
 		
-		JButton btnB = new JButton("2. Comunication");
-		btnB.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				p.print('B');
-				p.flush();
-			}
-		});
-		btnB.setBounds(159, 94, 105, 27);
-		frame.getContentPane().add(btnB);
-		
-		JButton btnC = new JButton("3. Packages");
-		btnC.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				p.print('C');
-				p.flush();
-			}
-		});
-		
-		btnC.setBounds(159, 150, 105, 27);
-		frame.getContentPane().add(btnC);
-		
-		JButton btnD = new JButton("4. Multipuropse");
-		btnD.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				p.print('D');
-				p.flush();
-			}
-		});
-		
-		btnD.setBounds(159, 204, 105, 27);
-		frame.getContentPane().add(btnD);
-		
-		JButton btnE = new JButton("5. Multipurpose");
-		btnE.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				p.print('E');
-				p.flush();
-			}
-		});
-		
-		btnE.setBounds(159, 258, 105, 27);
-		frame.getContentPane().add(btnE);
-		
-		btnA.addKeyListener(new KeyAdapter() {
+		btn.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyReleased(KeyEvent e) {
-				fKey(e);
-			}
-		});
-		
-		btnB.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				fKey(e);
-			}
-		});
-		
-		btnC.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				fKey(e);
-			}
-		});
-		
-		btnD.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				fKey(e);
-			}
-		});
-		
-		btnE.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent e) {
-				fKey(e);
+			public void keyReleased(KeyEvent e) 
+			{
+				char c = e.getKeyChar();
+				
+				if (c != '\n')
+					e.consume();
+				else
+					stampa();
 			}
 		});
 	}
-
 }
